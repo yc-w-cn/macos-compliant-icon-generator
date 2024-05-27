94fdfa6607208d1e7a78ca68ac6b5983d17fc64c
YC.W <contact@wangyuchen.cn>
2024-05-27 12:15:30 +0800

    feat: add gh-pages support

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+const isProduction = process.env.NODE_ENV === "production";
+const baseUrl = "/macos-compliant-icon-generator"
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: isProduction ? baseUrl : undefined,
+  assetPrefix: isProduction ? baseUrl : undefined,
+  images: { unoptimized: true },
+};
 
 export default nextConfig;
